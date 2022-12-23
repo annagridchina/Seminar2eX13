9e84d38f3b8d20dc4b99c1f419b06e994a23d5a1
anna <annagridchina@mail.ru>
2022-12-23 22:14:49 +0700

    решение. не могу убрать последню цифру, если число двух или однозначное

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или 
+// сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Start");
+
+int a = 7123;
+
+while  (a > 999) a = a / 10;
+Console.WriteLine(a % 10);
+    if (a < 100)
+   {
+        Console.WriteLine("третьей цифры нет");
+   }
+
+
+Console.WriteLine("END");   
+
+
+
+//if (a < 100)
+
+ // else
+   // {
+   //     Console.WriteLine("третьей цифры нет");
+    //}
+ 
